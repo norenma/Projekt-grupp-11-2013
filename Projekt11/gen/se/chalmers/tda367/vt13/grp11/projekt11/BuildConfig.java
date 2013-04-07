8741e1517a35e82e782d5546edd275ce9233238f
Roy Nard <roy.nard@gmail.com>
2013-04-07 19:56:02 +0200

    First commit: ADT default template.

---
+++ Projekt11/gen/se/chalmers/tda367/vt13/grp11/projekt11/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package se.chalmers.tda367.vt13.grp11.projekt11;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
