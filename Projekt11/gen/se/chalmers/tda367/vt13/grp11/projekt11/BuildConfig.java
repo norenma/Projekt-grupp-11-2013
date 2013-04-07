c497607cb86e3b081424acb643adcaaf2874ee72
Roy Nard <roy.nard@gmail.com>
2013-04-07 20:50:21 +0200

    Revert "First commit:"
    This reverts commit 8741e1517a35e82e782d5546edd275ce9233238f.

--- Projekt11/gen/se/chalmers/tda367/vt13/grp11/projekt11/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package se.chalmers.tda367.vt13.grp11.projekt11;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
