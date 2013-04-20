2fafdee40c16752feac318e0c03cceb42b73dc0e
Roy Nard <roy.nard@gmail.com>
2013-04-20 21:37:21 +0200

    A reward specific for a whole game of QuizWalk! (versus Rewards given for some Challenge)

---
+++ QuizWalk/src/se/chalmers/it12/tda367/vt13/grp11/quizwalk/model/QuizWalkGameReward.java
@@ -0,0 +1,13 @@
+package se.chalmers.it12.tda367.vt13.grp11.quizwalk.model;
+
+import com.google.common.base.Optional;
+
+public class QuizWalkGameReward extends Reward {
+
+	public QuizWalkGameReward(int score, String description,
+			Optional<Image> image) {
+		super(score, description, image);
+		// TODO Auto-generated constructor stub
+	}
+
+}
