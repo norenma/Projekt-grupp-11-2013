aef366ee4ddc62bde78a7379acd6b00ea433cea0
Johanna Hartman <Johannah40@hotmail.com>
2013-04-24 16:41:49 +0200

    Commit

---
+++ QuizWalk/src/se/chalmers/it12/tda367/vt13/grp11/quizwalk/activities/StartScreenActivity.java
@@ -0,0 +1,35 @@
+package se.chalmers.it12.tda367.vt13.grp11.quizwalk.activities;
+
+import se.chalmers.it12.tda367.vt13.grp11.quizwalk.R;
+import se.chalmers.it12.tda367.vt13.grp11.quizwalk.R.layout;
+import se.chalmers.it12.tda367.vt13.grp11.quizwalk.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.support.v4.app.NavUtils;
+
+public class StartScreenActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void changeScreen(View v){
+		Intent intent = new Intent(this, SecondScreen.class);
+		startActivity(intent);
+	}
+
+
+}
