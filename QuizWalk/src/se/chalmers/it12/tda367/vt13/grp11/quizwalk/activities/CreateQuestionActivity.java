77c6e7610a5ffce57445dc9085cfec823861cd32
Roy Nard <roy.nard@gmail.com>
2013-04-29 19:50:53 +0200

    organizing imports

--- QuizWalk/src/se/chalmers/it12/tda367/vt13/grp11/quizwalk/activities/CreateQuestionActivity.java
+++ QuizWalk/src/se/chalmers/it12/tda367/vt13/grp11/quizwalk/activities/CreateQuestionActivity.java
@@ -1,19 +1,8 @@
 package se.chalmers.it12.tda367.vt13.grp11.quizwalk.activities;
 
 import se.chalmers.it12.tda367.vt13.grp11.quizwalk.R;
-import se.chalmers.it12.tda367.vt13.grp11.quizwalk.R.id;
-import se.chalmers.it12.tda367.vt13.grp11.quizwalk.R.layout;
-import se.chalmers.it12.tda367.vt13.grp11.quizwalk.util.SystemUiHider;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 
 
