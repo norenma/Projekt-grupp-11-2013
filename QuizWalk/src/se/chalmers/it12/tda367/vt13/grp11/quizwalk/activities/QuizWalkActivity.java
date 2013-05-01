62455499853f806e02bb5d7b6c590bd14d2e1cc2
Hampus Forsvall <forzvall@gmail.com>
2013-05-01 23:27:27 +0200

    Added player location functionality. Still needs work but it's a start

--- QuizWalk/src/se/chalmers/it12/tda367/vt13/grp11/quizwalk/activities/QuizWalkActivity.java
+++ QuizWalk/src/se/chalmers/it12/tda367/vt13/grp11/quizwalk/activities/QuizWalkActivity.java
@@ -1,5 +1,7 @@
 package se.chalmers.it12.tda367.vt13.grp11.quizwalk.activities;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Iterator;
 
 import se.chalmers.it12.tda367.vt13.grp11.quizwalk.R;
@@ -8,7 +10,13 @@ import se.chalmers.it12.tda367.vt13.grp11.quizwalk.model.QuizWalkGame;
 import se.chalmers.it12.tda367.vt13.grp11.quizwalk.model.utils.Utilities;
 import temp.debug.norenma.TestRun;
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,8 +24,11 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
-public class QuizWalkActivity extends Activity {
+public class QuizWalkActivity extends Activity implements LocationListener {
 	private GoogleMap map;
+	private LocationManager locationManager;
+	private String provider;
+	private Location location;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +67,46 @@ public class QuizWalkActivity extends Activity {
 			}
 
 		});
+		
+		
+	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    provider = locationManager.getBestProvider(new Criteria(), false);
+	    location = locationManager.getLastKnownLocation(provider);
+		
+	    checkNotNull(location);
+	    
+	    onLocationChanged(location);
 
 
+	}
+	/* Request updates at startup */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		locationManager.requestLocationUpdates(provider, 400, 1, this);
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		Log.v("Debugg", location.getLongitude() + " " + location.getLatitude());
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()),3));	
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
 	}
 }
