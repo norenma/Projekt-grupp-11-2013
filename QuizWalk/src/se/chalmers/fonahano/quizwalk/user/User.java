9452dfba66e9c5f4bd615b054499fd6610be4ed7
Roy Nard <roy.nard@gmail.com>
2013-05-12 10:53:18 +0200

    Changed pkg names.

--- QuizWalk/src/se/chalmers/fonahano/quizwalk/user/User.java
+++ QuizWalk/src/se/chalmers/fonahano/quizwalk/user/User.java
@@ -1,4 +1,4 @@
-package se.chalmers.it12.tda367.vt13.grp11.quizwalk.model.user;
+package se.chalmers.fonahano.quizwalk.user;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
