22f5ed40828e463525906f4b08bd06ec0cbc6188
norenma <markus.a.noren@gmail.com>
2013-05-13 13:32:30 +0200

    - CreateQuestionFragment(CreateQuestionActivity) is now a fragment!

--- QuizWalk/src/se/chalmers/fonahano/quizwalk/activities/CreateGameActivity.java
+++ QuizWalk/src/se/chalmers/fonahano/quizwalk/activities/CreateGameActivity.java
@@ -1,25 +1,45 @@
 package se.chalmers.fonahano.quizwalk.activities;
 
 import se.chalmers.fonahano.quizwalk.R;
+import se.chalmers.fonahano.quizwalk.map.Location;
+import se.chalmers.fonahano.quizwalk.model.Challenge;
+import se.chalmers.fonahano.quizwalk.model.ChallengeReward;
+import se.chalmers.fonahano.quizwalk.model.Image;
 import se.chalmers.fonahano.quizwalk.model.QuizWalkGame;
 import se.chalmers.fonahano.quizwalk.model.QuizWalkGame.Builder;
+import se.chalmers.fonahano.quizwalk.model.StringAnswer;
+import se.chalmers.fonahano.quizwalk.model.StringQuestion;
 import android.app.Activity;
-import android.content.Intent;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.common.base.Optional;
 
+
+/***
+ * Class to show a map and let user insert questions into a custom
+ * quizwalk-game. 
+ * 
+ * @author Markus
+ *
+ */
 public class CreateGameActivity extends Activity {
+	
 	private QuizWalkGame.Builder builder=new Builder();
 	private GoogleMap map;
+	private LatLng activeLocation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_quiz_walk_game);
+		setContentView(R.layout.activity_create_game);
 		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 				.getMap();
 		
@@ -33,16 +53,57 @@ public class CreateGameActivity extends Activity {
 			@Override
 			public void onMapLongClick(LatLng arg0) {
 				map.addMarker(new MarkerOptions().position(arg0));
-				Intent intent=new Intent(CreateGameActivity.this, CreateQuestionActivity.class);
-				double[] location={arg0.latitude, arg0.longitude};
-				intent.putExtra(LOCATION_SERVICE, location);
-				startActivity(intent);
+
+				FragmentManager fragmentManager = getFragmentManager();
+				FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 				
+				CreateQuestionFragment cqa=new CreateQuestionFragment();
+				fragmentTransaction.add(R.id.fragment_container, cqa, "question");
+				fragmentTransaction.commit();
+				
+				activeLocation=arg0;
 				
 			}
 		});
-
-
-
+	}
+	
+	
+	/***
+	 * Creates a challenge out of collected data
+	 * and adds it to the game. 
+	 * Called when CreateQuestion-button is pushed. 
+	 * 
+	 * @param view
+	 */
+	public void CreateQuestion(View view){
+		Challenge.Builder build=new Challenge.Builder();
+		
+		//Gets question and answers
+		build.question(new StringQuestion((((EditText) this.findViewById(R.id.questionText)).getText().toString())));
+		build.correctAnswer(((EditText) this.findViewById(R.id.answer1)).getText().toString());
+		build.addIncorrectAnswer(new StringAnswer(((EditText) this.findViewById(R.id.answer2)).getText().toString()));
+		build.addIncorrectAnswer(new StringAnswer(((EditText) this.findViewById(R.id.answer3)).getText().toString()));
+		build.addIncorrectAnswer(new StringAnswer(((EditText) this.findViewById(R.id.answer4)).getText().toString()));
+		build.challengeReward(new ChallengeReward(100, " ", Optional.<Image>absent()));
+	
+		//Gets the coordinates
+		Location location=new Location(activeLocation.latitude, activeLocation.longitude, " ", Optional.<Image>absent());
+		build.addLocation(location);
+		
+		
+		//Builds the challenge
+		Challenge c=build.build();
+		
+		//Adds question to quizwalk
+		this.builder.addChallenge(c);
+		
+		//Gets back to the map-view
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		
+		fragmentTransaction.hide(fragmentManager.findFragmentByTag("question"));
+		fragmentTransaction.commit();
+		
+		
 	}
 }
---
+++ QuizWalk/src/se/chalmers/fonahano/quizwalk/activities/CreateQuestionFragment.java
@@ -0,0 +1,31 @@
+package se.chalmers.fonahano.quizwalk.activities;
+
+
+
+import se.chalmers.fonahano.quizwalk.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+/***
+ * A class to collect data from user to create a StringQuestion.
+ * Class is called from CreateGameActivite and it«s intent should already contain
+ * a LatLng-location
+ * 
+ * @author Markus 
+ *
+ */
+public class CreateQuestionFragment extends Fragment {
+	
+	   @Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+	        Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_question, container, false);
+	}
+}
