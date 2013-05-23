3ca34f0e2ce03e083a482e91cf87d57ac9650b15
Hampus Forsvall <forzvall@gmail.com>
2013-05-23 14:29:50 +0200

    Will now initiate three quizwalks for debug purposes

--- QuizWalk/src/se/chalmers/fonahano/quizwalk/activities/GameMenuActivity.java
+++ QuizWalk/src/se/chalmers/fonahano/quizwalk/activities/GameMenuActivity.java
@@ -3,6 +3,7 @@ package se.chalmers.fonahano.quizwalk.activities;
 import se.chalmers.fonahano.quizwalk.R;
 import se.chalmers.fonahano.quizwalk.interfaces.C;
 import se.chalmers.fonahano.quizwalk.model.QuizWalkGame;
+import temp.debug.tortal.DebugFactory;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -27,6 +28,9 @@ public class GameMenuActivity extends Activity {
 		
 		//Check if Google play service APK is installed
 		ensureGooglePlayServicesIsEnabled();
+		
+		//Adds a few random QuizWalks to the db. For debuging
+		DebugFactory.addRandomListOfQuizWalksToDB();
 		
 	}
 	
