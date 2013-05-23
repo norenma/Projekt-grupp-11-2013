fdc931bb60464355e7ca2db4800761d21b5d11fe
Hampus Forsvall <forzvall@gmail.com>
2013-05-23 14:20:18 +0200

    Added a few more debug quizwalks

--- QuizWalk/src/temp/debug/tortal/DebugFactory.java
+++ QuizWalk/src/temp/debug/tortal/DebugFactory.java
@@ -1,10 +1,23 @@
 package temp.debug.tortal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import se.chalmers.fonahano.quizwalk.model.Challenge;
 import se.chalmers.fonahano.quizwalk.model.QuizWalkGame;
 
 public class DebugFactory {
 
+	public static List<QuizWalkGame> getListOfQuizWalks(){
+		List<QuizWalkGame> list = new ArrayList<QuizWalkGame>();
+		
+		list.add(getRandomTortalChalmersQuizWalkGame1());
+		list.add(getRandomTortalChalmersQuizWalkGame2());
+		list.add(getRandomTortalChalmersQuizWalkGame3());
+		
+		return list;
+		
+	}
 	public static QuizWalkGame getRandomTortalChalmersQuizWalkGame1() {
 		QuizWalkGame.Builder builder = new QuizWalkGame.Builder();
 
@@ -22,7 +35,7 @@ public class DebugFactory {
 		Challenge c2 = new Challenge.Builder().description("2: Kiosken")
 			.location(57.687107d,
 				11.981138d)
-			.question("Vilken film är bäst?")
+			.question("Vilken film ï¿½r bï¿½st?")
 			.correctAnswer("Mad Max 2: The Road Warrior")
 			.addIncorrectAnswer("Avatar")
 			.addIncorrectAnswer("Bad Boys 2")
@@ -37,4 +50,74 @@ public class DebugFactory {
 			.addChallenge(c2)
 			.build();
 	}
+	
+	public static QuizWalkGame getRandomTortalChalmersQuizWalkGame2() {
+		QuizWalkGame.Builder builder = new QuizWalkGame.Builder();
+
+		// Challenges
+		Challenge c1 = new Challenge.Builder().description("1: Korsningen")
+			.location(57.689854d,
+				11.978145d)
+			.question("What is really cool?")
+			.correctAnswer("42")
+			.addIncorrectAnswer("Bieber")
+			.addIncorrectAnswer("North Korea")
+			.addIncorrectAnswer("aina")
+			.build();
+
+		Challenge c2 = new Challenge.Builder().description("2: Kiosken")
+			.location(57.697107d,
+				11.001138d)
+			.question("Vilken film ï¿½r bï¿½st?")
+			.correctAnswer("Mad Max 2: The Road Warrior")
+			.addIncorrectAnswer("Avatar")
+			.addIncorrectAnswer("Bad Boys 2")
+			.addIncorrectAnswer("Fast & Furious 8")
+			.build();
+
+		// Return QuizWalkGame
+		return builder.name("tortal test game :: "
+				+ (int) (1 + Math.random() * 99))
+			.description("Two challenges in chalmers")
+			.addChallenge(c1)
+			.addChallenge(c2)
+			.build();
+	}
+	
+	public static QuizWalkGame getRandomTortalChalmersQuizWalkGame3() {
+		QuizWalkGame.Builder builder = new QuizWalkGame.Builder();
+
+		// Challenges
+		Challenge c1 = new Challenge.Builder().description("1: Korsningen")
+			.location(57.619854d,
+				11.908145d)
+			.question("What is really cool?")
+			.correctAnswer("42")
+			.addIncorrectAnswer("Bieber")
+			.addIncorrectAnswer("North Korea")
+			.addIncorrectAnswer("aina")
+			.build();
+
+		Challenge c2 = new Challenge.Builder().description("2: Kiosken")
+			.location(57.687107d,
+				11.981138d)
+			.question("Vilken film ï¿½r bï¿½st?")
+			.correctAnswer("Mad Max 2: The Road Warrior")
+			.addIncorrectAnswer("Avatar")
+			.addIncorrectAnswer("Bad Boys 2")
+			.addIncorrectAnswer("Fast & Furious 8")
+			.build();
+
+		// Return QuizWalkGame
+		return builder.name("tortal test game :: "
+				+ (int) (1 + Math.random() * 99))
+			.description("Two challenges in chalmers")
+			.addChallenge(c1)
+			.addChallenge(c2)
+			.build();
+	}
+
 }
+
+
+
