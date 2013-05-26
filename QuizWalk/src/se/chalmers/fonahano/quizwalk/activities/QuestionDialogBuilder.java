dc0e28a1b2c209943a89441d27d58f52a0f50857
Hampus Forsvall <forzvall@gmail.com>
2013-05-26 13:19:18 +0200

    deleted hardcoded strings

--- QuizWalk/src/se/chalmers/fonahano/quizwalk/activities/QuestionDialogBuilder.java
+++ QuizWalk/src/se/chalmers/fonahano/quizwalk/activities/QuestionDialogBuilder.java
@@ -7,7 +7,7 @@ import se.chalmers.fonahano.quizwalk.interfaces.Answer;
 import se.chalmers.fonahano.quizwalk.model.Challenge;
 import se.chalmers.fonahano.quizwalk.model.QuizWalkGame.ChallengeState;
 import se.chalmers.fonahano.quizwalk.model.StateSingleton;
-import android.R;
+import se.chalmers.fonahano.quizwalk.R;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -43,10 +43,10 @@ public class QuestionDialogBuilder extends AlertDialog.Builder {
 			if (correctAnswerIndex == which) {
 				// TODO: Change to static string resources
 				StateSingleton.INSTANCE.getActiveQuizWalk().get().setChallengeState(challenge, ChallengeState.COMPLETED);
-				showDialog("Correct!");
+				showDialog(QuestionDialogBuilder.this.getContext().getResources().getString(R.string.correct_answer_prompt));
 			} else {
 				StateSingleton.INSTANCE.getActiveQuizWalk().get().setChallengeState(challenge, ChallengeState.FAILED);
-				showDialog("Wrong...");
+				showDialog(QuestionDialogBuilder.this.getContext().getResources().getString(R.string.incorrect_answer_prompt));
 			}
 		}
 
